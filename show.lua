6e4cf5c0ff919021c0d8bd814a52dd78293a9443
ThanhTuoi852123 <51827435+ThanhTuoi852123@users.noreply.github.com>
2023-04-07 23:47:02 +0700

    Update show.lua

--- show.lua
+++ show.lua
@@ -7,13 +7,13 @@
     "Accept": "*/*", 
     "Cookie": "", 
     "Host": "httpbin.org", 
-    "Syn-Fingerprint": "aa57fe332635a1785354b4ae0c2de33da31946e9f07818bb2e7ac42cca9790a74c1fcc99e5f0bdc6830b7b91774d576e9a7b2812cfb7e9c34801d73635959e08", 
-    "Syn-User-Identifier": "9c7a6eaf31072ad4c7176c874995fec2097d93c2c2bd5e071e1fe26d1c78a0a7242f3e621af8b71a37c0c500364acdfa929d439eb4a2373a5d9778c67cc793b4", 
+    "Syn-Fingerprint": "ae2e2bc20c1277a0bf2f58c2972d320ebbe0e27665601548ebc7341fd5b535b3bb7956a9ace9eb7c25fc76976ad8442f1227bd49cc1f18abb735df70d57bbcbc", 
+    "Syn-User-Identifier": "1023cfa9f6e15bd3f3a2ba9c13468afa9aab9b5717479aeac7945d7db5b6dd2c85d4eda06757613b7c8378eb51ee29f0d93e7ce64d5f5b27cef14b9d14340f8f", 
     "User-Agent": "synx/v2.23.7b", 
-    "X-Amzn-Trace-Id": "Root=1-64304798-384118c1609a93fd30b0195b"
+    "X-Amzn-Trace-Id": "Root=1-643048f3-7fdf876f090b911c7684f040"
   }, 
   "json": null, 
   "method": "GET", 
-  "origin": "1.55.38.168", 
+  "origin": "27.79.221.72", 
   "url": "https://httpbin.org/anything"
 }
