2bff8a6ee6b30bf481e48eec8ca3c83ebf9c9042
ThanhTuoi852123 <51827435+ThanhTuoi852123@users.noreply.github.com>
2023-07-25 15:35:17 +0700

    Update spirit.lua

--- spirit.lua
+++ spirit.lua
@@ -1,1 +1,7 @@
-
+if game.PlaceId == 6284583030 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/checkrampet.lua"))() --script check ram pet
+elseif game.PlaceId == 2753915549 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/checkram.lua"))()
+else
+print("Chưa Support")
+end
