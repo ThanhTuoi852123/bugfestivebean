a4f127423f0c424f76ed0f72063d31ea8b9a128b
ThanhTuoi852123 <51827435+ThanhTuoi852123@users.noreply.github.com>
2023-07-26 18:56:19 +0700

    Update spirit.lua

--- spirit.lua
+++ spirit.lua
@@ -1,6 +1,6 @@
 if game.PlaceId == 6284583030 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/checkrampet.lua"))() --script check ram pet
-elseif game.PlaceId == 2753915549 then
+elseif placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/checkram.lua"))()
 else
 print("Chưa Support")
