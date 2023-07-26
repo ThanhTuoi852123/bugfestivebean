6f0d891f9554f1d7a1f27e98dcfff778c3faad2b
ThanhTuoi852123 <51827435+ThanhTuoi852123@users.noreply.github.com>
2023-07-26 19:00:04 +0700

    Update spirit.lua

--- spirit.lua
+++ spirit.lua
@@ -3,5 +3,6 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfe
 elseif game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/checkram.lua"))()
 else
-print("Chưa Support")
+print("Chưa Support1")
 end
+
