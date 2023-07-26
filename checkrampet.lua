b561389eca5ea332fd80a3f9f0130f99a7ec4797
ThanhTuoi852123 <51827435+ThanhTuoi852123@users.noreply.github.com>
2023-07-26 17:01:25 +0700

    Update checkrampet.lua

--- checkrampet.lua
+++ checkrampet.lua
@@ -95,11 +95,13 @@ spawn(function()
         end)
     end
 end)
-
+spawn(function()
 local player = game.Players.LocalPlayer
 player.CharacterRemoving:Connect(function()
     sendStatusToServer(script_key,"OFFLINE")
 end)
+end)
+spawn(function()
 game.CoreGui.RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(child)
    if child.Name == "ErrorPrompt" then
        print(2)
@@ -107,6 +109,8 @@ game.CoreGui.RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(child)
        game:Shutdown()
    end
 end)
+end)
+spawn(function()
 local GuiService = game:GetService('GuiService')
 GuiService.ErrorMessageChanged:Connect(function()
    local ErrorCode = GuiService:GetErrorCode().Value
@@ -115,4 +119,5 @@ GuiService.ErrorMessageChanged:Connect(function()
 	sendStatusToServer(script_key,"OFFLINE")
 	game:Shutdown()
    end
+end)
 end)
