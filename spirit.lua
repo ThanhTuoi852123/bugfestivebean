b6559abdd9800ffbaff408a80f0bf788cfb993e5
ThanhTuoi852123 <51827435+ThanhTuoi852123@users.noreply.github.com>
2023-08-13 20:23:09 +0700

    Update spirit.lua

--- spirit.lua
+++ spirit.lua
@@ -1,10 +1,6 @@
---if game.PlaceId == 6284583030 then
---loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/checkrampet.lua"))()
---elseif game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
---loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/checkram.lua"))()
---else
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/anothergame.lua"))()
---end
+
 
   
 
