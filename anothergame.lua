9d4db6d1a68b35f9315cc5be1ec5d56b4b41750e
ThanhTuoi852123 <51827435+ThanhTuoi852123@users.noreply.github.com>
2023-08-13 11:03:52 +0700

    Update anothergame.lua

--- anothergame.lua
+++ anothergame.lua
@@ -4,10 +4,10 @@ repeat wait() until game.Players.LocalPlayer.Character
 
 local HttpRequest = (syn and syn.request) or http and http.request or http_request or (fluxus and fluxus.request) or request
 local HttpService = game:GetService("HttpService")
-function accountonl()
+function accountonl(status)
 local data = {
     name = game.Players.LocalPlayer.Name,
-    status = "ONLINE"
+    status = status
 }
 
 local response = HttpRequest(
@@ -25,21 +25,24 @@ local sss = true
 local GuiService = game:GetService('GuiService')
 while sss do
     pcall(function()
-        	accountonl()
+        	accountonl("ONLINE")
         	wait(60)
     end)
     local ErrorCode = GuiService:GetErrorCode().Value
     if ErrorCode >= Enum.ConnectionError.DisconnectErrors.Value then
 		start = false
+		accountonl("OFFLINE")
    end
     for _,v in pairs(game.CoreGui.RobloxPromptGui.promptOverlay:GetChildren()) do
 		  if v.Name == "ErrorPrompt" then
-			  sss = false
+			sss = false
+			accountonl("OFFLINE")
 		  end
     end
 end
 game.CoreGui.RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(child)
    if child.Name == "ErrorPrompt" then
        sss = false
+       accountonl("OFFLINE")
    end
 end)
