63f4fd89e544290133adb2dc4920a6cf9ccbaa03
ThanhTuoi852123 <51827435+ThanhTuoi852123@users.noreply.github.com>
2023-08-14 21:59:55 +0700

    Update anothergame.lua

--- anothergame.lua
+++ anothergame.lua
@@ -24,29 +24,21 @@ end
 local sss = true
 local GuiService = game:GetService('GuiService')
 spawn(function()
-while true do
- --   pcall(function()
-        local data = {
-            name = tostring(game.Players.LocalPlayer.Name),
-            status = "ONLINE"
-        }
-        print("Dang chay 1")
-        local response = HttpRequest(
-           {
-               Url = "http://localhost:5000/post",  -- This website helps debug HTTP requests
-               Method = "POST",
-               Headers = {
-                   ["Content-Type"] = "application/json"  -- When sending JSON, set this!
-               },
-               Body = HttpService:JSONEncode(data)
-           }
-        )
-        print("Dang chay")
-        for _,v in pairs(response) do
-            print(_,v)
-        end
---    end)
-        wait(10)
+while sss do
+       pcall(function()
+            accountonl("ONLINE")
+       end)
+    for _,v in pairs(game.CoreGui.RobloxPromptGui.promptOverlay:GetChildren()) do
+		if v.Name == "ErrorPrompt" then
+			start = false
+		end
+	end
+	local ErrorCode = GuiService:GetErrorCode().Value
+	if ErrorCode >= Enum.ConnectionError.DisconnectErrors.Value then
+		start = false
+   	end
+    wait(60)
+    
 end
 end)
 print("ONLINE.")
