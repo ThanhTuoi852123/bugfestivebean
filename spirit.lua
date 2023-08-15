78608063e330088fc4ac45f672c22dc11ecd66f4
ThanhTuoi852123 <51827435+ThanhTuoi852123@users.noreply.github.com>
2023-08-16 00:06:53 +0700

    Update spirit.lua

--- spirit.lua
+++ spirit.lua
@@ -1,6 +1,6 @@
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/anothergame.lua"))()
-
-
-  
-
+if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/bloxngu.lua"))()
+elseif game.PlaceId == 6284583030 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/petngu.lua"))()
+end
+end
