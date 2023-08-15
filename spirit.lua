6a84cc91a368b0f9d694c6abcb4c4c037032207d
ThanhTuoi852123 <51827435+ThanhTuoi852123@users.noreply.github.com>
2023-08-16 00:13:39 +0700

    Update spirit.lua

--- spirit.lua
+++ spirit.lua
@@ -1,7 +1,9 @@
---if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
---loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/bloxngu.lua"))()
---elseif game.PlaceId == 6284583030 then
---loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/petngu.lua"))()
---end
---end
+if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/bloxngu.lua"))()
+elseif game.PlaceId == 6284583030 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/petngu.lua"))()
+end
+else
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/anothergame.lua"))()
+end
+
