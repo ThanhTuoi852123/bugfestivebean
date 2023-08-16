78d132b444137d40694a9b75147ad95a19773e80
ThanhTuoi852123 <51827435+ThanhTuoi852123@users.noreply.github.com>
2023-08-16 13:43:34 +0700

    Update spirit.lua

--- spirit.lua
+++ spirit.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/038b1aec7a2f028ba631988e12dae152.lua"))()
 elseif game.PlaceId == 6284583030 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/petngu.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/52ef01653d6d7594a730611f9bcd3773.lua"))()
 end
 else
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ThanhTuoi852123/bugfestivebean/main/anothergame.lua"))()
